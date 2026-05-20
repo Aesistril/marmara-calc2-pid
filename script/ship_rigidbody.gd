e7f1a4669f68feca44bd5bbb8f94d21e0aaaab83
AdraOk <110841620+AdraOk@users.noreply.github.com>
2026-05-20 18:50:05 +0300

    Update ship_rigidbody.gd

--- script/ship_rigidbody.gd
+++ script/ship_rigidbody.gd
@@ -3,35 +3,33 @@ extends RigidBody3D
 var station_col: Area3D
 var shuttle_col: Area3D
 
-var integral_abs_err: Vector3 = Vector3(0, 0, 0)
-
 var integral: Vector3 = Vector3(0, 0, 0)
 var integral_zvel: float
 var prev_distance: Vector3 = Vector3(0, 0, 0)
 var prev_error_zvel: float
+var target_velocity_z: float 
 
-const Kp = 200.0
-const Ki = 0.00001
-const Kd = 9400.0
-const Kall = 1
+const speedFactor = 32.0
 
-const engineLimit = 1000
+const Kp = 400*(speedFactor*speedFactor)
+const Ki = 0.0001
+const Kd = 40000*speedFactor
 
-const KZp = 200.0
-const KZi = 0.00001
-const KZd = 9400.0
-const KZall = 1
+const engineLimit = 100000
+const epsilon = 0.01
+
+const KZp = 120*speedFactor
+const KZi = 0.0001
+const KZd = 1250*speedFactor
 
 const ship_randpos_mult = 50
-const ship_randvel_mult = 0.5
+const ship_randvel_mult = 25
 const station_randpos_mult = 50
 
 var cam_mode: int = 0
 var camera_speed = 0.02
 
 var station_target_pos = Vector3(0, 0, -21)
-
-var z_overshoot: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -45,53 +43,42 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var distance = station_col.global_position - shuttle_col.global_position
-	
 	################## Space Station Placement ##################
 	$"../station".global_position += \
 		((station_target_pos - $"../station".position)*1*delta).clamp(
 		Vector3(-1, -1, -1), Vector3(1, 1, 1))
-		
-	###################### Detect Overshoot #####################
-	if distance.z > z_overshoot:
-		z_overshoot = distance.z
-		$"../CanvasLayer/z_overshoot".text = \
-			"Approach Overshoot: " + str(z_overshoot)
-			
-	##################### Integral Abs Err ######################
-	integral_abs_err += abs(distance)*delta
-	$"../CanvasLayer/int_abs_err".text = \
-		"Integral Absolute Error: " + str(integral_abs_err)
 	
 	######################  Space Ship PID ######################
+	var distance = station_col.global_position - shuttle_col.global_position
+
 	# take the numerial integrals and derivative of the distance
 	var derivative = (distance - prev_distance)/delta
 	integral += distance * delta
-	constant_force = Kall*(Kp * distance + Ki * integral + Kd * derivative)
-	var target_velocity_z: float
-
-
-	if abs(constant_force.x) < 100 && abs(constant_force.y) < 100:
-		target_velocity_z = distance.z * 1
-	else:
-		target_velocity_z = 0
+	constant_force = (Kp * distance + Ki * integral + Kd * derivative)
 	
+	
+	target_velocity_z = distance.z*speedFactor
+		
 	var error_zvel = target_velocity_z - linear_velocity.z
 	var derivative_zvel = (error_zvel - prev_error_zvel)/delta
 	integral_zvel += error_zvel * delta
-	constant_force.z = KZall* \
-		(KZp * error_zvel + KZi * integral_zvel + KZd * derivative_zvel)
+	constant_force.z = (KZp * error_zvel + KZi * integral_zvel + KZd * derivative_zvel)
+		
+		
+	# save the current distance and z velocity to be used in the next frame
+	prev_distance = distance
+	prev_error_zvel = error_zvel	
+		
+	print(constant_force)
+	print(distance)
+	
 	
 	constant_force = constant_force.clamp(
 		-Vector3(engineLimit,engineLimit,engineLimit), 
 		Vector3(engineLimit,engineLimit,engineLimit))
 
-	# save the current distance and z velocity to be used in the next frame
-	prev_distance = distance
-	prev_error_zvel = error_zvel
 	
-	$"../CanvasLayer/distance".text = "Distance: " + str(distance)
-	$"../CanvasLayer/shuttle_forces".text = "Forces on Shuttle: " + str(constant_force)
+	
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
@@ -118,5 +105,4 @@ func _process(delta):
 			camera_anchor.rotate_z(camera_speed)
 		if Input.is_action_pressed("ui_down"):
 			camera_anchor.rotate_z(-camera_speed)
-
 	$"../earth".rotate_y(delta*0.01)
